dd6ea430f6bdef2d6b9a1092524b1c63697fa8c7
Taweesak.Y <36216629+WeZaPro@users.noreply.github.com>
2024-09-06 21:18:45 +0700

    Update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -89,7 +89,7 @@ export default {
       // (window.location.href = "https://liffcontrol.onrender.com/liffweb"),
       //   "_blank";
       //window.open("https://liffcontrol.onrender.com/liffweb", "_blank");
-      window.open("https://1f7e-223-204-235-121.ngrok-free.app/liffweb")
+      window.open("https://schoolshopliffapp.onrender.com/liffweb")
     },
     // siteB() {
     //   console.log("openLineChat--> ");
